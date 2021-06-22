4688405609e1f92476d1880ebceb297a4fa275e4
dudchenko610 <rusland610@.gmail.com>
2021-06-22 18:52:26 +0300

    Request log middleware was added

---
+++ CrazyTasks/Middlewares/ConsoleReguestLogMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Middlewares
+{
+    public class ConsoleLogMiddleware
+    {
+        private RequestDelegate _next;
+
+        public ConsoleLogMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("                                         _");
+            Console.WriteLine(context.Request.Path);
+            Console.WriteLine("                                         _");
+
+
+            Console.WriteLine("__________________ Request Headers __________________");
+            foreach (var e in context.Request.Headers)
+            {
+                Console.WriteLine("[] " + e.Key + "   :   " + e.Value);
+            }
+
+
+            Console.WriteLine("__________________ Response Headers __________________");
+            foreach (var e in context.Response.Headers)
+            {
+                Console.WriteLine("[] " + e.Key + "   :   " + e.Value);
+            }
+
+            await _next.Invoke(context);
+        }
+
+    }
+}
