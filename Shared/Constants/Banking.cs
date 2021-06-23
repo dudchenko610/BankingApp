608387e60d6ceb4857f5c4540c59ba4146e544a7
dudchenko610 <rusland610@.gmail.com>
2021-06-23 17:47:21 +0300

    Banking constant class was marked as a static

--- Shared/Constants/Banking.cs
+++ Shared/Constants/Banking.cs
@@ -8,7 +8,7 @@ namespace Shared
     {
         public static partial class Errors
         {
-            public class Banking
+            public static class Banking
             {
                 public const string INCORRECT_PRICE_FORMAT = "Price can't have more than 2 decimal places";
                 public const string INCORRECT_MONTH_NUMBER = "Months number should be positive non-zero integer value";
@@ -18,7 +18,7 @@ namespace Shared
 
         public static partial class Routes
         {
-            public class Banking
+            public static class Banking
             {
                 public const string CALCULATE_DEPOSITE = "calculateDeposite";
             }
