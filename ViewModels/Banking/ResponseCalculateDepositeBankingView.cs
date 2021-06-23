c33f81533b06d7d223e778266be95b5c2dfc0d0e
dudchenko610 <rusland610@.gmail.com>
2021-06-23 16:58:13 +0300

    Add lost files

---
+++ ViewModels/Banking/ResponseCalculateDepositeBankingView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ViewModels.Banking
+{
+    public class ResponseCalculateDepositeBankingView
+    {
+        public ResponseCalculateDepositeBankingView()
+        {
+            PerMonthInfos = new List<ResponseCalculateDepositeBankingViewItem>();
+        }
+        public IList<ResponseCalculateDepositeBankingViewItem> PerMonthInfos { get; set; }
+    }
+}
---
+++ ViewModels/Banking/ResponseCalculateDepositeBankingViewItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ViewModels.Banking
+{
+    public class ResponseCalculateDepositeBankingViewItem
+    {
+        public int MonthNumber { get; set; }
+        public decimal TotalMonthSum { get; set; }
+        public int Percents { get; set; }
+    }
+}
