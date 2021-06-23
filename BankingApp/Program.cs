157f12422f191f2bc73e3b034ea84c089da0ec18
dudchenko610 <rusland610@.gmail.com>
2021-06-23 12:07:28 +0300

    Renamed project

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CrazyTasks
+namespace BankingApp
 {
     public class Program
     {
--- BankingApp/Startup.cs
+++ BankingApp/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CrazyTasks
+namespace BankingApp
 {
     public class Startup
     {
