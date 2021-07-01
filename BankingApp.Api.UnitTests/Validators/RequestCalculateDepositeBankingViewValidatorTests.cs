0765d303d5c6fa1f43814e6cd8edac703657bddb
dudchenko610 <rusland610@.gmail.com>
2021-07-01 10:32:17 +0300

    RequestViewModelValidator's test inputs were replaced to private method

--- BankingApp.Api.UnitTests/Validators/RequestCalculateDepositeBankingViewValidatorTests.cs
+++ BankingApp.Api.UnitTests/Validators/RequestCalculateDepositeBankingViewValidatorTests.cs
@@ -13,51 +13,48 @@ namespace BankingApp.Api.UnitTests.Validators
     {
         private RequestCalculateDepositeBankingViewValidator _validator;
 
+        private IList<RequestCalculateDepositeBankingView> RequestCalculateDepositeBankingViews()
+        {
+            return new List<RequestCalculateDepositeBankingView>
+            {
+                new RequestCalculateDepositeBankingView { DepositeSum = -1.0m, CalculationFormula = 0, MonthsCount = 1, Percents = 1 },
+                new RequestCalculateDepositeBankingView { DepositeSum = 1, CalculationFormula = 0, MonthsCount = -1, Percents = 1 },
+                new RequestCalculateDepositeBankingView { DepositeSum = -1.05m, CalculationFormula = 0, MonthsCount = 0, Percents = 105 },
+                new RequestCalculateDepositeBankingView { DepositeSum = 1, CalculationFormula = 0, MonthsCount = 1, Percents = 0 }
+            };
+        }
+
         [SetUp]
         public void SetUp()
         {
             _validator = new RequestCalculateDepositeBankingViewValidator();
         }
 
-        private IList<RequestCalculateDepositeBankingView> RequestCalculateDepositeBankingViews
-        {
-            get
-            {
-                return new List<RequestCalculateDepositeBankingView>
-                {
-                    new RequestCalculateDepositeBankingView { DepositeSum = -1.0m, CalculationFormula = 0, MonthsCount = 1, Percents = 1 },
-                    new RequestCalculateDepositeBankingView { DepositeSum = 1, CalculationFormula = 0, MonthsCount = -1, Percents = 1 },
-                    new RequestCalculateDepositeBankingView { DepositeSum = -1.05m, CalculationFormula = 0, MonthsCount = 0, Percents = 105 },
-                    new RequestCalculateDepositeBankingView { DepositeSum = 1, CalculationFormula = 0, MonthsCount = 1, Percents = 0 }
-                };
-            }
-        }
-
         [Test]
         public void Validate_DepositeSumLessThanZero_ValidErrorMessage()
         {
-            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews[0]);
+            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews()[0]);
             validateResult.Errors.Any(x => x.ErrorMessage == Constants.Errors.Banking.IncorrectPriceFormat).Should().BeTrue();
         }
 
         [Test]
         public void Validate_MonthCountLessThanZero_ValidErrorMessage()
         {
-            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews[1]);
+            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews()[1]);
             validateResult.Errors.Any(x => x.ErrorMessage == Constants.Errors.Banking.IncorrectMonthFormat).Should().BeTrue();
         }
 
         [Test]
         public void Validate_PercentBiggerThan100_ValidErrorMessage()
         {
-            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews[2]);
+            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews()[2]);
             validateResult.Errors.Any(x => x.ErrorMessage == Constants.Errors.Banking.IncorrectPercentNumber).Should().BeTrue();
         }
 
         [Test]
         public void Validate_PercentLessThan1_ValidErrorMessage()
         {
-            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews[3]);
+            var validateResult = _validator.Validate(RequestCalculateDepositeBankingViews()[3]);
             validateResult.Errors.Any(x => x.ErrorMessage == Constants.Errors.Banking.IncorrectPercentNumber).Should().BeTrue();
         }
     }
