3a7263df4b17dde14385cbf3376afc4390a53de7
dudchenko610 <rusland610@.gmail.com>
2021-07-12 09:55:46 +0300

    switchCounter removed from LoaderService

--- BankingApp.UI.Core/Services/LoaderService.cs
+++ BankingApp.UI.Core/Services/LoaderService.cs
@@ -5,29 +5,16 @@ namespace BankingApp.UI.Core.Services
 {
     public class LoaderService : ILoaderService
     {
-        private int _switchCounter;
         public event Action<bool> OnLoaderSwitch;
-
-        public LoaderService()
-        {
-            _switchCounter = 0;
-        }
 
         public void SwitchOff()
         {
-            _switchCounter--;
-            _switchCounter = _switchCounter < 0 ? 0 : _switchCounter;
-
-            if (_switchCounter == 0)
-                OnLoaderSwitch?.Invoke(false);
+            OnLoaderSwitch?.Invoke(false);
         }
 
         public void SwitchOn()
         {
-            int prevSwitch = _switchCounter;
-            _switchCounter++;
-            if (prevSwitch == 0)
-                OnLoaderSwitch?.Invoke(true);
+            OnLoaderSwitch?.Invoke(true);
         }
     }
 }
